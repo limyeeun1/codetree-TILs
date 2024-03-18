c6b3dfd48f2ad9de1f132ed978391e738c4b72f2
limyeeun1 <88433276+limyeeun1@users.noreply.github.com>
2024-03-18 22:36:23 +0900

    Title: 문자열 계속 지우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/문자열 계속 지우기/keep-removing-string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string A,B;
+    cin >> A >> B;
+    int l = B.length();
+    int i=0;
+    for(i=0;i<A.length();i++){
+        // cout << A << ","<<"i" << i<< A.substr(i,l) << "compare" << B << endl;
+        if (A.substr(i,l) == B){
+            A = A.substr(0,i) + A.substr(i+l,A.length()-i-l);
+            // cout << A << "and" << i << endl;
+            i = i-2;
+        }
+        // cout << i;
+    }
+    cout << A ;
+
+    return 0;
+}
