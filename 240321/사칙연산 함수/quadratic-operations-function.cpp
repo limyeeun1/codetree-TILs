cd358e8d4f049709ca9a5697d7a0a9c96a252f80
limyeeun1 <88433276+limyeeun1@users.noreply.github.com>
2024-03-21 22:09:26 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/사칙연산 함수/quadratic-operations-function.cpp
+++ 240321/사칙연산 함수/quadratic-operations-function.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
-void plu(int a, int c){cout << a+c;}
-void mi(int a, int c){cout << a-c;}
-void multip(int a, int c){cout << a*c;}
-void division(int a, int c){cout << (int) a/c;}
+int plu(int a, int c){return a+c;}
+int mi(int a, int c){return a-c;}
+int multip(int a, int c){return a*c;}
+int division(int a, int c){return (int) a/c;}
 int main() {
-    int a,c;
+    int a,c,ans;
     char o;
+    
     cin >> a >> o >> c;
-    if (o=='+'){plu(a,c);}
-    else if (o=='-'){mi(a,c);}
-    else if (o=='/'){division(a,c);}
-    else if (o=='*'){multip(a,c);}
-    else {cout << "False";}
+    if (o=='+'){ans=plu(a,c);}
+    else if (o=='-'){ans=mi(a,c);}
+    else if (o=='/'){ans=division(a,c);}
+    else if (o=='*'){ans=multip(a,c);}
+    else {cout << "False";return 0;}
+    printf("%d %c %d = %d",a,o,c,ans);
     return 0;
 }
