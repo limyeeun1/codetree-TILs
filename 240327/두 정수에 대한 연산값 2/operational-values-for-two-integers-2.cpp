f9069a853d303d6093ddd36110a053d4de1808b4
limyeeun1 <88433276+limyeeun1@users.noreply.github.com>
2024-03-27 20:15:06 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240327/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void make(int &a,int &b){
+    a +=10;
+    b *=2;
+}
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a<b){
+        make(a,b);
+        cout << a << " " << b;
+        return 0;}
+    else {
+        make(b,a);
+        cout << b << " " << a;
+        return 0;}
+
+
+}
