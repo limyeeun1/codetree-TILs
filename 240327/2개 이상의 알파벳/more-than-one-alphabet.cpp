44329760e6b577fe796f6a90c985d1b524a54ea2
limyeeun1 <88433276+limyeeun1@users.noreply.github.com>
2024-03-27 20:05:38 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240327/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int check_(string &str){
+    string::iterator iter;
+    for(iter=str.begin();iter!=str.end()-1;iter++){
+        if (*iter==*(iter+1)){cout << "Yes";return 0;}
+    }
+    cout << "No";
+    return 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    check_(str);
+    return 0;
+}
