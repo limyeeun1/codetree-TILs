da2e1d641299840b0fd182290fea4bd6d50e3485
limyeeun1 <88433276+limyeeun1@users.noreply.github.com>
2024-03-28 20:40:03 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int find_max(int n, int m){
+    int v;
+    cin >> v;
+    if (n==1){return v;}
+    return max(find_max(n-1,v),m);
+
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int v;
+    cin >> v;
+    cout << find_max(5,v);
+
+
+    return 0;
+}
